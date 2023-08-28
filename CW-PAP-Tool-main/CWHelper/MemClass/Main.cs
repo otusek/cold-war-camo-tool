9c8a686937db18c9ada7c92ca30bde1bdb03dfc5
otusek <143442990+otusek@users.noreply.github.com>
2023-08-28 21:31:52 +0200

    Add files via upload

---
+++ CW-PAP-Tool-main/CWHelper/MemClass/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MemClass
+{
+    public partial class Main : Form
+    {
+        private Memory m;
+        private enum Status
+        {
+            SUCCESS,
+            SIG_NOT_FOUND,
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+
+            // Check if "BlackOpsColdWar" process is running
+            Process[] processes = Process.GetProcessesByName("BlackOpsColdWar");
+            if (processes.Length == 0)
+            {
+                MessageBox.Show("Error: Start BOCW!", "Process Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+
+            m = new Memory("BlackOpsColdWar");
+        }
+
+        private void camoBtn_Click(object sender, EventArgs e)
+        {
+            Output output = new Output(outputBox);
+            Camos result = new Camos(camoBox.Text);
+            output.Clear();
+
+            byte camo = result.getCamo();
+
+            switch (setCamo(camo))
+            {
+                case Status.SUCCESS:
+                    output.AddText("[+] " + camoBox.Text + " set successfully!");
+                    break;
+                case Status.SIG_NOT_FOUND:
+                    output.AddText("[x] Sig not found!");
+                    output.AddText("[x] Make sure to set shards as one of your camos!");
+                    break;
+                default:
+                    output.AddText("[x] An unknown error has occured");
+                    break;
+            }
+        }
+
+        private Status setCamo(byte camo)
+        {
+            long scan = m.FindBytes("01 ? ? ? ? ? ? ? 03 ? ? ? ? ? ? ? 0F A3 ? AF", m.BaseAddress + 0x16000000, m.BaseAddress + 0x18000000, true).FirstOrDefault<long>();
+
+            if (scan == default)
+                return Status.SIG_NOT_FOUND;
+
+            m.Write<byte>(scan, camo);
+            return Status.SUCCESS;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CW-PAP-Tool-main/README.md
@@ -0,0 +1,9 @@
+# CW-PAP-Tool
+The method for this has been public for a hell of a long time now, so I figured I would take the initiative and finally release a public tool for this
+- link for original tutorial: https://www.unknowncheats.me/forum/3408246-post2246.html
+
+The program IS NOT mine i just edited few lines of code, original author is linked in unknowncheats post
+
+Subscribe to my channel <3
+
+Also discord with console services coming soon!
